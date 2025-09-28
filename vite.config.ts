10656c72f64d95915ad4c5a08b2771febfb171bf
Rithick2024 <rithickraghu79@gmail.com>
2025-09-28 12:20:44 +0530

    name update : Notes-App-1.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Notes-App/',
+  base: '/Notes-App-1.0/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
